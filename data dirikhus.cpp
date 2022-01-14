a476573220f39f550e7dfda4eee80ef703471d96
Yakin-saestu <97730555+Yakin-saestu@users.noreply.github.com>
2022-01-14 16:25:47 +0700

    TugasAlpro

---
+++ data dirikhus.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	char nama[25];
+	int nim;
+	char prodi[50];
+	
+	printf("\nNUR KHUSNAINIL YAQIN",nama);
+	printf("\nProdi Teknik Informatika",prodi);
+	printf("\n07.1.2021.0013",nim);
+	
+	return 0;
+}
